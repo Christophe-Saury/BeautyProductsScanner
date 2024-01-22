70a2419cbc8fadfd10406507ff31a7a68061206d
Christophe SAURY <95085659+Christophe-Saury@users.noreply.github.com>
2024-01-22 16:58:08 +0100

    Delete BeautyProductsScanner-back directory

--- BeautyProductsScanner-back/src/auth/guards/accessToken.guards.ts
+++
@@ -1,7 +0,0 @@
-import { ExecutionContext, Injectable, CanActivate, SetMetadata } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
-
-@Injectable()
-export class AccessTokenGuard extends AuthGuard('jwt') { }
